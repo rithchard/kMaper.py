c81f053047b243d0ac1e3133023c8b103bd07ecc
rithchard <rithchard@gmail.com>
2013-07-06 01:58:22 -0700

    Update kMaper.py

--- kMaper.py
+++ kMaper.py
@@ -1,2 +1,21 @@
-  def menu
-  
+"""
+kMaper.py is a tool to test in dfd
+
+"""
+import Helper
+'''
+' Starting
+'''
+
+
+#urlBase =  sys.argv[1]
+urlBase = 'www.thiseas-taekwondo.gr'
+
+if urlBase.startswith('http') or urlBase.startswith('www'):
+  SITE = Helper.getSITE(urlBase)
+	Helper.empezando(SITE)
+	Helper.checkMotor(SITE)
+	Helper.getInfoJoomla(SITE)
+else:
+	Helper.newbie('La URL es Invalida!','Ejemplo de uso:')
+	exit(1)
