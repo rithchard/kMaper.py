8cb86c1acb4d5b647bd6f055001f274824845bcb
rithchard <rithchard@gmail.com>
2013-07-27 09:55:48 -0700

    Update kMaper.py
    use it! && try it!

--- kMaper.py
+++ kMaper.py
@@ -1,21 +1,26 @@
 """
-kMaper.py is a tool to test in dfd
+kMaper.py is a tool to test in cms's
+
 
 """
-import Helper
+import Helper,sys
 '''
-' Starting
+' Usar: $ python kMaper.py http://www.splatgore.de/index.php
 '''
 
 
-#urlBase =  sys.argv[1]
-urlBase = 'www.thiseas-taekwondo.gr'
+urlBase =  sys.argv[1]
+#urlBase = 'www.thiseas-taekwondo.gr'
+#urlBase = 'http://www.rcv-reichenbach.net/index.php?option=com_mytube&view=videos&Itemid=94'
+#urlBase = 'http://www.splatgore.de/index.php'
+#urlBase = 'http://www.johnled.gr/index.php'
+#urlBase = 'http://www.diegos-canela.de/index.php'
 
 if urlBase.startswith('http') or urlBase.startswith('www'):
-  SITE = Helper.getSITE(urlBase)
+	SITE = Helper.getSITE(urlBase)
 	Helper.empezando(SITE)
-	Helper.checkMotor(SITE)
-	Helper.getInfoJoomla(SITE)
+	Helper.checkIfJoomla(SITE)
 else:
 	Helper.newbie('La URL es Invalida!','Ejemplo de uso:')
 	exit(1)
+
